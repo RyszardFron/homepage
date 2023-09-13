bf4a67052671344b4fc13f648143af6440b1bb59
RyszardFron <119079388+RyszardFron@users.noreply.github.com>
2023-09-13 08:15:10 +0000

    added function to change navigation background

--- js/script.js
+++ js/script.js
@@ -6,14 +6,22 @@
   const toggleBackground = () => {
     const body = document.querySelector(".body");
     const themeName = document.querySelector(".button__text");
-        
+
     body.classList.toggle("dark");
     themeName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
+  };
+
+  const navigationChangeBack = () => {
+    const navigationBackground = document.querySelectorAll(".navigation__link");
+    navigationBackground.forEach(navigationBack => {
+      navigationBack.classList.toggle("navigation__dark");
+    })
   };
 
   const init = () => {
     const changeBackgroundButton = document.querySelector(".button");
     changeBackgroundButton.addEventListener("click", toggleBackground);
+    changeBackgroundButton.addEventListener("click", navigationChangeBack);
 
     welcome()
   };
